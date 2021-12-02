a18c75dc05fedcf51a281eb357c1b7c3ae55aaa4
Diego Fidalgo <diego-fidalgo@hotmail.com>
2021-12-02 19:36:36 -0300

    Projeto iniciado

---
+++ .eslintrc.cjs
@@ -0,0 +1,66 @@
+/**
+ * @type {import('eslint').Linter.Config}
+ */
+const config = {
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: [
+        'eslint:recommended',
+    ],
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module'
+    },
+    plugins: [
+        'svelte3'
+    ],
+    globals: {
+        NodeJS: 'readonly',
+        $$Generic: 'readonly',
+        svelte: 'readonly',
+    },
+    rules: {
+        'indent': [
+            'error',
+            4,
+            { SwitchCase: 1 }
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single',
+            { allowTemplateLiterals: true }
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        'no-var': 'error',
+        'eqeqeq': [
+            'error',
+            'always'
+        ]
+    },
+    overrides: [
+        {
+            files: ['*.svelte'],
+            processor: 'svelte3/svelte3'
+        },
+        {
+            files: ['*.cjs'],
+            env: {
+                node: true,
+            }
+        }
+    ],
+    settings: {
+        // 'svelte3/ignore-styles': () => true
+    }
+};
+
+module.exports = config;
